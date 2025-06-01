492fa70954483b0527246f4bb7778b6428b183c9
vimalwynn <vimalwyn@gmail.com>
2025-06-01 19:33:28 +0300

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -26,7 +26,7 @@ export default function Products() {
     
     if (hasHalfStar) {
       stars.push(
-        <Star key="half" className="h-4 w-4 text-amber-400" />
+        <Star key="half\" className="h-4 w-4 text-amber-400" />
       );
     }
     
